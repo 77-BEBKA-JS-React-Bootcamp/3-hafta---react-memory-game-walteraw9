e7a2ebf75b03f6bd3f70b31c9596ca91f82dd4ab
walteraw9 <ayberk.aladagli@hotmail.com>
2021-03-07 23:14:45 +0300

    commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Angular PokeDEX APP
---
+++ server.js
@@ -0,0 +1,32 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
+
+
+const forceSSL = function() {
+    return function (req, res, next) {
+      if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(
+         ['https://', req.get('Host'), req.url].join('')
+        );
+      }
+      next();
+    }
+  }
+
+
+
+  app.use(forceSSL());
